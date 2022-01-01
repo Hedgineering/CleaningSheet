46a9e40b9f6e93a7f9ade9611d61763400584167
Rahul Hegde <rahulhegde@Rahuls-MacBook-Pro.local>
2022-01-01 16:45:10 -0500

    updated index.ts to correctly source the config.env file

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config({ path: '../config.env' });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({ path: __dirname + '/../config.env' });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5000;
--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,10 @@
-require('dotenv').config({path: '../config.env'})
+import dotenv from 'dotenv'
+// configure the source .env file to be from absolute path to our config.env
+dotenv.config({path: __dirname + '/../config.env'})
 
 import express from 'express'
 const app = express()
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000 // Use environment variable as priority
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))

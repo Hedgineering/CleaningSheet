30e9512a9469024b464a14a11b390d4ce22201ab
Rahul Hegde <rahulhegde@Rahuls-MacBook-Pro.local>
2022-01-01 15:58:00 -0500

    moved git from client to outer dir

---
+++ server/index.ts
@@ -0,0 +1,8 @@
+require('dotenv').config({path: '../config.env'})
+
+import express from 'express'
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
